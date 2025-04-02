d7d7bafebc8ea935a517392eeafa11d7e11315e5
Nguyên Võ <vokhoinguyen2017@gmail.com>
2025-04-02 20:20:14 +0700

    add system prompt 2

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
     # Chat input
     if user_input := st.chat_input("Type your message..."):
         system_prompt='''Your are an finance assistant. If the prompt is not related to finance. 
-        You should answer like this "I would not answer anything related to finance", and if anyone ask about your origin, just say that you are created by Andy:'''
+        You should answer like this "I would not answer anything not related to finance", and if anyone ask about your origin, just say that you are created by Andy:'''
         # Add user message to history
         st.session_state.messages.append({"role": "user", "content": user_input})
         with st.chat_message('user'):
